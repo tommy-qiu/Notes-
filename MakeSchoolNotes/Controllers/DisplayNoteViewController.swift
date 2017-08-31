a10890a42d2e0f3b1d8fb3918d6b40f9e848491c
tommy-qiu <tq326@nyu.edu>
2017-08-30 21:58:11 -0400

    This is a finished Notes App using CoreData

---
+++ MakeSchoolNotes/Controllers/DisplayNoteViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DisplayNoteViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class DisplayNoteViewController: UIViewController {
+    var note: Note?
+    
+    @IBOutlet weak var noteTitleTextField: UITextField!
+    @IBOutlet weak var noteContentTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 1
+        if let note = note {
+            // 2
+            noteTitleTextField.text = note.title
+            noteContentTextView.text = note.content
+        } else {
+            // 3
+            noteTitleTextField.text = ""
+            noteContentTextView.text = ""
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "save" {
+            // if note exists, update title and content
+            let note = self.note ?? CoreDataHelper.newNote()
+            note.title = noteTitleTextField.text ?? ""
+            note.content = noteContentTextView.text ?? ""
+            note.modificationTime = Date() as NSDate
+            CoreDataHelper.saveNote()
+        }
+    }
+}
+
---
+++ MakeSchoolNotes/Helpers/CoreDataHelper.swift
@@ -0,0 +1,52 @@
+//
+//  CoreDataHelper.swift
+//  MakeSchoolNotes
+//
+//  Created by Tommy Qiu on 7/5/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class CoreDataHelper {
+    static let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    static let persistentContainer = appDelegate.persistentContainer
+    static let managedContext = persistentContainer.viewContext
+    //static methods will go here
+    static func newNote() ->Note {
+        let note = NSEntityDescription.insertNewObject(forEntityName: "Note", into: managedContext) as! Note
+        return note
+    }
+    static func saveNote() {
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save \(error)")
+        }
+    }
+    
+        static func delete(note: Note) {
+            managedContext.delete(note)
+            saveNote()
+        }
+    static func retrieveNotes() -> [Note] {
+        let fetchRequest = NSFetchRequest<Note>(entityName: "Note")
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            return results.sorted(by: { (($0.modificationTime!) as Date) > ($1.modificationTime! as Date)})
+            
+            
+//            return results.sorted { (note: Note ,noteTwo: Note) -> Bool in
+//                let date1 = note.modificationTime! as Date
+//                let date2 = noteTwo.modificationTime! as Date
+//                return date1 > date2
+    
+            
+        } catch let error as NSError {
+            print("Could not fetch \(error)")
+        }
+        return []
+    }
+    }
